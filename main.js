1a0196d5d11f9a283ba3cd3cf1e6345797032286
jerobejrobinson <jerobejrobinson@gmail.com>
2023-10-19 12:47:02 -0500

    Added an animation library and added about section as well as where section

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+AOS.init();
 function toggleNavButton(x) {
     x.classList.toggle('change');
     document.querySelector('#navLinks').classList.toggle('change');
+    document.querySelector('body').classList.toggle('change')
 }
+(() => {
+    const navToggle = document.querySelector('#navToggle')
+    navToggle.addEventListener('click', (e) => toggleNavButton(e.target))
+
+    const links = Array.from(document.querySelectorAll('#navLinks > a'))
+    links.forEach(link => {
+        link.addEventListener('click', (e) => toggleNavButton(e.target))
+    })
+})()
